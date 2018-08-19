96ef63dfcc0fe21ec144a27d627b1e991e2c1f5d
mwutti <michaelwutti@gmail.com>
2018-08-19 10:01:02 +0200

    Todo refactor label_map

--- csv-metadata.py
+++ csv-metadata.py
@@ -1,20 +1,20 @@
 import csv
+import detector as detector
 
+# class VideoSequence:
+#     def __init__(self, id, start_time, duration, type, match_id, perspective, stream_file, stream_timestamp,
+#                  utc_timestamp):
+#         self.id = id
+#         self.start_time = start_time
+#         self.duration = duration
+#         self.type = type
+#         self.match_id = match_id
+#         self.perspective = perspective
+#         self.stream_file = stream_file
+#         self.stream_timestamp = stream_timestamp
+#         self.utc_timestamp = utc_timestamp
 
-class VideoSequence:
-    def __init__(self, id, start_time, duration, type, match_id, perspective, stream_file, stream_timestamp,
-                 utc_timestamp):
-        self.id = id
-        self.start_time = start_time
-        self.duration = duration
-        self.type = type
-        self.match_id = match_id
-        self.perspective = perspective
-        self.stream_file = stream_file
-        self.stream_timestamp = stream_timestamp
-        self.utc_timestamp = utc_timestamp
-
-
+video_path = 'D:/gamestory18-data/train_set'
 def timestamp_to_sec(timestamp):
     split_timestamp = timestamp.split(':')
     stream_timestamp_sec = int(split_timestamp[0]) * 3600 + int(split_timestamp[1]) * 60 + int(split_timestamp[2])
@@ -33,10 +33,9 @@ def sec_to_timestamp(sec):
 
 header_row = {}
 matches = {}
-
+# separate csv-data between different matches
 with open('data/metadata.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    # separate between different different matches
     line = 0
     for row in reader:
         if line > 0:
@@ -53,11 +52,14 @@ with open('data/metadata.csv', newline='') as csvfile:
 
 # At this point there is a list of rows for each match in matches
 
-# lowest timestamp in stream file
+# Search for lowest and highest timestamps in stream files
 lowest_timestamp_sec = 360000
 highest_timestamp_sec = 0
 lowest_timestamp_row = ''
 highest_timestamp_row = ''
+highest_timestamp_map = {}
+lowest_timestamp_map = {}
+
 for match in matches:
     for row in matches[match]:
         # find lowest timestamp in p11 stream
@@ -76,13 +78,25 @@ for match in matches:
             if row[3] == 'match_start':
                 match_start = row[7]
                 print('start for match ' + match + " @" + match_start)
-    print('lowest timestampt for match ' + match + ' ' + str(lowest_timestamp_sec) + ", " + sec_to_timestamp(lowest_timestamp_sec))
-    print(lowest_timestamp_row)
-    print("---")
-    print('highest timestampt for match ' + match + ' ' + str(highest_timestamp_sec) + ", " + sec_to_timestamp(highest_timestamp_sec))
-    print(highest_timestamp_row)
-    print("---match end---")
+    # print('lowest timestampt for match ' + match + ' ' + str(lowest_timestamp_sec) + ", " + sec_to_timestamp(lowest_timestamp_sec))
+    # print(lowest_timestamp_row)
+    # print("---")
+    # print('highest timestampt for match ' + match + ' ' + str(highest_timestamp_sec) + ", " + sec_to_timestamp(highest_timestamp_sec))
+    # print(highest_timestamp_row)
+    # print("---match end---")
+    highest_timestamp_map[match] = highest_timestamp_row
+    lowest_timestamp_map[match] = lowest_timestamp_row
     lowest_timestamp_sec = 360000
     highest_timestamp_sec = 0
+    lowest_timestamp_row = ''
+    highest_timestamp_row = ''
 
 #TODO search for trophies in lowest_timestamp - 20min ; highest_timestamp + 20min in matches
+#search from lowest timestamp + 20 min
+for match in lowest_timestamp_map:
+    begin_sec = int(lowest_timestamp_map[match][1]) + 10
+    end_sec = begin_sec + 2
+    # detector.detect(video_path + "/"  + lowest_timestamp_map[match][6], begin_sec, end_sec, match)
+    class_map = detector.detect(video_path + "/"  + '2018-03-04_P11.mp4', begin_sec, end_sec, match)
+    print(class_map)
+    # print("search from "  + sec_to_timestamp(begin_sec) + " to " + sec_to_timestamp(end_sec) + " for trophies in m match " + match)
--- detector.py
+++ detector.py
@@ -4,14 +4,7 @@ import cv2
 import label_map_util
 import numpy as np
 import tensorflow as tf
-video_url = 'D:\gamestory18-data/train_set/2018-03-04_P11.mp4'
-# video_url = 'video/p11_cut.mp4'
-cap = cv2.VideoCapture(video_url)
-fps = cap.get(cv2.CAP_PROP_FPS)
-
-#jump to 8:20 in video
-frame_pos = 30000*60
-cap.set(cv2.CAP_PROP_POS_FRAMES, frame_pos)
+import visualization_utils
 
 TRAINING_DIR = 'training'
 DATA_DIR = 'data'
@@ -25,72 +18,117 @@ PATH_TO_CKPT = TRAINING_DIR + '/' + MODEL_NAME + '/frozen_inference_graph.pb'
 PATH_TO_LABELS = TRAINING_DIR + '/' + MODEL_NAME + '/' + LABEL_MAP
 
 NUM_CLASSES = 90
+max_boxes_to_draw = 20
+class_map = {}
 
-# ## Load a (frozen) Tensorflow model into memory.
 
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+def detect(video_path, begin_sec, end_sec, video_name):
+    print("Start detecting from " + video_name)
+    # init openCV capture
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+    # TODO hardcoded 60FPS
+    frame_pos_start = begin_sec * 60
+    current_frame = frame_pos_start
+    frame_pos_end = end_sec * 60
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                            use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+    # Set start of capture
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_pos_start)
 
-with detection_graph.as_default():
-    with tf.Session(graph=detection_graph) as sess:
-        while True:
-            ret, image_np = cap.read()
-            pos_msec = cap.get(cv2.CAP_PROP_POS_MSEC)
+    # Load a (frozen) Tensorflow model into memory.
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
 
-            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-            image_np_expanded = np.expand_dims(image_np, axis=0)
-            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-            # Each box represents a part of the image where a particular object was detected.
-            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
-            scores = detection_graph.get_tensor_by_name('detection_scores:0')
-            classes = detection_graph.get_tensor_by_name('detection_classes:0')
-            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-            # Actual detection.
-            (boxes, scores, classes, num_detections) = sess.run(
-                [boxes, scores, classes, num_detections],
-                feed_dict={image_tensor: image_np_expanded})
+    # ## Loading label map
+    # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
 
-            # Visualization of the results of a detection.
-            # vis_util.visualize_boxes_and_labels_on_image_array(
-            #     image_np,
-            #     np.squeeze(boxes),
-            #     np.squeeze(classes).astype(np.int32),
-            #     np.squeeze(scores),
-            #     category_index,
-            #     use_normalized_coordinates=True,
-            #     line_thickness=8)
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                                use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
 
-            #detect cups and trophies
-            min_score_thresh = .5
-            scores = np.squeeze(scores)
-            classes = np.squeeze(classes).astype(np.int32)
-            for i in range(boxes.shape[0]):
-                if scores is None or scores[i] > min_score_thresh:
-                    display_str = ''
-                    # if not skip_labels:
-                    if classes[i] in category_index.keys():
-                        class_name = category_index[classes[i]]['name']
-                        print(class_name + '@ ' + str(pos_msec))
-                    else:
-                        class_name = 'N/A'
-                    display_str = str(class_name)
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            while current_frame <= frame_pos_end:
+                ret, image_np = cap.read()
+                current_frame += 1
+                pos_msec = cap.get(cv2.CAP_PROP_POS_MSEC)
 
-            cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                # Actual detection.
+                (boxes, scores, classes, num_detections) = sess.run(
+                    [boxes, scores, classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
+
+                # Visualization of the results of a detection.
+                # visualization_utils.visualize_boxes_and_labels_on_image_array(
+                #     image_np,
+                #     np.squeeze(boxes),
+                #     np.squeeze(classes).astype(np.int32),
+                #     np.squeeze(scores),
+                #     category_index,
+                #     use_normalized_coordinates=True,
+                #     line_thickness=8)
+
+                # detect cups and trophies
+                min_score_thresh = .5
+                scores = np.squeeze(scores)
+                classes = np.squeeze(classes).astype(np.int32)
+                boxes = np.squeeze(boxes)
+
+                for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+                    if scores is None or scores[i] > min_score_thresh:
+                        # print('current frame: ' + str(current_frame))
+                        # print(scores)
+                        display_str = ''
+                        # if not skip_labels:
+                        if classes[i] in category_index.keys():
+                            class_name = category_index[classes[i]]['name']
+                            if class_name not in class_map:
+                                class_map[class_name] = []
+                                class_map[class_name].append(current_frame)
+                                class_map[class_name].append(pos_msec)
+                                class_map[class_name].append(1)                #nr of matches for class name
+                            else:
+                                class_map[class_name][0] = current_frame
+                                class_map[class_name][1] = pos_msec
+                                class_map[class_name][2] += 1               # nr of matches for class name
+                        else:
+                            class_name = 'N/A'
+
+                        # print(class_map)
+
+                        # for class_name in class_map:
+                            # output_string = output_string + class_name + str(class_map[class_name]) + ','
+
+
+                        # print(output_string)
+
+                # Todo delete
+                cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
+
+    print("End detecting from " + video_name)
+    return class_map
+
+
+
+
+
