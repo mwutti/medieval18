b64d73107b92969331c8a09a87c6491308a8e5f0
mwutti <michaelwutti@gmail.com>
2019-05-27 17:20:46 +0200

    score for test-set calculated

--- score_extractor.py
+++ score_extractor.py
@@ -11,8 +11,9 @@ output_dir = "scores/"
 
 metadata_csv = util.read_metadata_csv()
 round_detector = round_detector.RoundDetector()
-print('hello')
-with open(output_dir + 'scores.csv', mode='w', newline='') as scores_file:
+
+# create scores file for training set
+with open(output_dir + 'scores-training.csv', mode='w', newline='') as scores_file:
     scores_writer = csv.writer(scores_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     scores_writer.writerow(['match', 'round', 'ct', 'terrorists'])
 
@@ -20,6 +21,31 @@ with open(output_dir + 'scores.csv', mode='w', newline='') as scores_file:
         # 10.json is not correct!
         if i != 10:
             json_file = open(base_dir + '/timelines/' + str(i) + '.json')
+            all_rounds_data = TimelineReader.preprocess(json.load(json_file))
+            score_map = TimelineReader.get_score_map_for_match(all_rounds_data)
+
+            roundNr = 0
+            scoreCT, scoreTerrorist = 0, 0
+
+            for score in score_map:
+                roundNr += 1
+                for entry in score:
+                    if score[entry]['ingameTeam'] == 'CT':
+                        scoreCT = score[entry]['score']
+                    else:
+                        scoreTerrorist = score[entry]['score']
+
+                scores_writer.writerow([i, roundNr, scoreCT, scoreTerrorist])
+
+# create scores file for test set
+with open(output_dir + 'scores-test.csv', mode='w', newline='') as scores_file:
+    scores_writer = csv.writer(scores_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    scores_writer.writerow(['match', 'round', 'ct', 'terrorists'])
+
+    for i in range(1, 5):
+        # 10.json is not correct!
+        if i != 10:
+            json_file = open(base_dir + '/timelines/test/' + str(i) + '.json')
             all_rounds_data = TimelineReader.preprocess(json.load(json_file))
             score_map = TimelineReader.get_score_map_for_match(all_rounds_data)
 
